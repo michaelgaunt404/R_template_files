10f15fdacfcc72ca29437dd83fb1a5b31bfe9546
michaelgaunt404 <mike.gaunt.404@gmail.com>
2021-07-14 15:39:43 -0700

    first commit, dumping scripts from local

---
+++ README.md
@@ -0,0 +1,9 @@
+#R_template_files
+> A curated list of custom R scripts
+
+This repo containts custom scripts written by [Mike Gaunt](https://github.com/michaelgaunt404). 
+
+## Contents of this repo:
+
+- Style guide templates for scripts and utility scripts which hold custom fuctions (that you write!)
+- Functions with specific tasks like functions that enhance existing packages
---
+++ zz_emptyScriptTemplate_CODE.R
@@ -0,0 +1,38 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# This is [[insert description here - what it does/solve]]
+#
+# By: mike gaunt, michael.gaunt@wsp.com
+#
+# README: [[insert brief readme here]]
+#-------- [[insert brief readme here]]
+#
+# *please use 80 character margins
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#library set-up=================================================================
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#content in this section should be removed if in production - ok for dev 
+
+#path set-up====================================================================
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#content in this section should be removed if in production - ok for dev 
+
+#source helpers/utilities=======================================================
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#content in this section should be removed if in production - ok for dev 
+
+#source data====================================================================
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#content in this section should be removed if in production - ok for dev 
+#area to upload data with and to perform initial munging
+#please add test data here so that others may use/unit test these scripts
+
+#SECTION NAME===================================================================
+#use this header to make demarcations/section in code [delete this line]
+#short description
+
+
+
+#script end=====================================================================
